8db2663189bca0fd41a9d6ace2fee45e3d62dd21
romelmain <romelherreratgk@gmail.com>
2024-11-18 19:56:45 -0400

    Consume el Api school supplies

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 $(document).ready(function () {});
+
+function prueba() {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    username: $("#username").val(),
+    password: $("#password").val(),
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  let aa = {};
+
+  aa = fetch("http://localhost:8080/api/users/login", requestOptions)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json;
+      } else {
+        console.log("Respuesta de red OK pero respuesta HTTP no OK");
+      }
+    })
+    .catch(function (error) {
+      console.log("Hubo un problema con la petición Fetch:" + error.message);
+    });
+
+  console.log(aa);
+}
