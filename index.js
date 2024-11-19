852bff8330dd3d5d96dd1d4fdf3ecf4e0e3534f5
romelmain <romelherreratgk@gmail.com>
2024-11-19 19:31:24 -0400

    Se guarda el user id en el localstorage

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  let userid = 0;
+  userid = parseInt(localStorage.getItem("userid"));
 
-function prueba() {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  const raw = JSON.stringify({
-    username: $("#username").val(),
-    password: $("#password").val(),
-  });
-
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
-
-  let aa = {};
-
-  aa = fetch("http://localhost:8080/api/users/login", requestOptions)
-    .then(function (response) {
-      if (response.ok) {
-        return response.json;
-      } else {
-        console.log("Respuesta de red OK pero respuesta HTTP no OK");
-      }
-    })
-    .catch(function (error) {
-      console.log("Hubo un problema con la petición Fetch:" + error.message);
-    });
-
-  console.log(aa);
-}
+  if (userid > 0) {
+    console.log("aaa");
+  } else {
+    console.log("bbb");
+    $(location).attr("href", "http://localhost:5500/login/login.html");
+  }
+});
---
+++ login/login.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {});
+
+async function login() {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    username: $("#username").val(),
+    password: $("#password").val(),
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      "http://localhost:8080/api/users/login",
+      requestOptions
+    );
+    if (response.ok) {
+      const json = await response.json();
+      console.log(json);
+      localStorage.setItem("userid", json.id);
+      $(location).attr("href", "http://localhost:5500/index.html");
+    } else {
+      console.log(response.status);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
